a4c7dd2977235ea0d9e8ddbbef089b2d4391289a
Szymon <szymon.lurka@interia.pl>
2021-10-15 13:02:25 +0200

    initial commit

---
+++ apps/tmt-stroop-test/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tmt-stroop-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'tmt-stroop-test';
+}
