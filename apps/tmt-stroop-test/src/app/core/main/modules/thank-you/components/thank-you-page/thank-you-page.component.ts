27d80d0a99a25c10a78c350d442d61ea8375ed37
Michal Duszak <drduszak@gmail.com>
2021-10-27 17:43:41 +0200

    clear localstorage on thankyou page

--- apps/tmt-stroop-test/src/app/core/main/modules/thank-you/components/thank-you-page/thank-you-page.component.ts
+++ apps/tmt-stroop-test/src/app/core/main/modules/thank-you/components/thank-you-page/thank-you-page.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import { LocalStorage } from '../../../../../../../lib/services/local-storage/local-storage';
 @Component({
   selector: 'tmt-stroop-test-thank-you-page',
   templateUrl: './thank-you-page.component.html',
   styleUrls: ['./thank-you-page.component.scss']
 })
-export class ThankYouPageComponent {
+export class ThankYouPageComponent implements OnInit {
+constructor(private localStorage: LocalStorage) {
+
+}
+  ngOnInit() {
+    this.localStorage.clear();
+  }
 }
